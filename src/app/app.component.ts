17e3c46d5b1fdfd5de56378539c3c5064264e170
Kristjan Vingel <kristjan.vingel@protonmail.ch>
2024-01-21 14:49:07 +0200

    env setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { environment } from '../environments/environment.development';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'a-labs';
+  url = environment.apiUrl
 }

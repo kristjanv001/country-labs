6904f9e834ea6fd26fc1f071f50a04a1321e34de
Kristjan Vingel <kristjan.vingel@protonmail.ch>
2024-01-30 00:04:35 +0200

    mvp done. created about page copy text. added somepadding for the accordion.

--- src/app/ui/ui-accordion-helm/src/lib/hlm-accordion-trigger.directive.ts
+++ src/app/ui/ui-accordion-helm/src/lib/hlm-accordion-trigger.directive.ts
@@ -16,7 +16,7 @@ export class HlmAccordionTriggerDirective {
 	private readonly _userCls = signal<ClassValue>('');
 	protected _computedClass = computed(() =>
 		hlm(
-			'w-full focus-visible:outline-none text-sm focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-2 flex flex-1 items-center justify-between font-medium underline-offset-4 hover:underline [&[data-state=open]>[hlmAccordionIcon]]:rotate-180 [&[data-state=open]>[hlmAccIcon]]:rotate-180',
+			'py-2 w-full focus-visible:outline-none text-sm focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-2 flex flex-1 items-center justify-between font-medium underline-offset-4 hover:underline [&[data-state=open]>[hlmAccordionIcon]]:rotate-180 [&[data-state=open]>[hlmAccIcon]]:rotate-180',
 			this._userCls(),
 		),
 	);
